db2b7776f415c792fcb08618fd285b1a882e7c37
ruvzi <zarevo@ukr.net>
2020-08-13 11:31:32 +0300

    fix conditions nil keys

--- lib/pg_partitioned/monkey_patch_awesome_nested_set.rb
+++ lib/pg_partitioned/monkey_patch_awesome_nested_set.rb
@@ -7,8 +7,8 @@ module CollectiveIdea #:nodoc:
         module ClassMethods
           def nested_set_scope(options = {})
             options = {:order => { order_column => :asc }}.merge(options)
-            conditions = options[:conditions] || {}
-            collection = try(:partitioned?) && conditions.keys.include?(partition_key) ?
+            conditions = options[:conditions]
+            collection = try(:partitioned?) && conditions && conditions.keys.include?(partition_key) ?
                            from_partition(partition_key_value(conditions)) :
                            self
             collection.where(conditions).order(options.delete(:order))
